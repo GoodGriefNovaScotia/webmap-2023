d7fe8c89647185baa1330c550ffbf73f42b3ae35
GoodGriefNovaScotia <goodgriefnovascotia@gmail.com>
2023-07-09 15:06:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,38 @@ var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/w
 	ext: 'jpg'
 });
 
+var markerClusterOptions = {
+  spiderfyOnMaxZoom: true,
+  showCoverageOnHover: false,
+  zoomToBoundsOnClick: true
+};
+
+// // Set up Layer Groups
+// // Categorical
+// var allEvents = L.markerClusterGroup(markerClusterOptions);
+// var eventsPets = L.featureGroup.subGroup(allEvents);
+// var eventsParents = L.featureGroup.subGroup(allEvents);
+// var eventsSpouses = L.featureGroup.subGroup(allEvents);
+// var eventsChildren = L.featureGroup.subGroup(allEvents);
+// var eventsFriends = L.featureGroup.subGroup(allEvents);
+// var eventsOther = L.featureGroup.subGroup(allEvents);
+// // In-person / Hybrid
+// var eventsInPerson = L.markerClusterGroup(markerClusterOptions);
+// var eventsHybrid = L.featureGroup.subGroup(allEvents);
+
+// // Set up Layer Groups
+// // Categorical
+// var allEvents = L.markerClusterGroup(markerClusterOptions);
+// var eventsPets = L.markerClusterGroup(markerClusterOptions);
+// var eventsParents = L.markerClusterGroup(markerClusterOptions);
+// var eventsSpouses = L.markerClusterGroup(markerClusterOptions);
+// var eventsChildren = L.markerClusterGroup(markerClusterOptions);
+// var eventsFriends = L.markerClusterGroup(markerClusterOptions);
+// var eventsOther = L.markerClusterGroup(markerClusterOptions);
+// // In-person / Hybrid
+// var eventsInPerson = L.markerClusterGroup(markerClusterOptions);
+// var eventsHybrid = L.markerClusterGroup(markerClusterOptions);
+
 // Set up Layer Groups
 // Categorical
 var allEvents = L.layerGroup();
@@ -27,11 +59,14 @@ var eventsOther = L.layerGroup();
 var eventsInPerson = L.layerGroup();
 var eventsHybrid = L.layerGroup();
 
+var layerSupport = new L.MarkerClusterGroup.LayerSupport();
+
 // Initialize map
 var map = L.map('map', {
   center: [44.6923, -62.6572],
   zoom: 7,
-  layers: allEvents
+  layers: allEvents,
+  maxZoom: 24
 });
 
 // Default Base Map
@@ -42,6 +77,49 @@ var defaultBaseMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png
 defaultBaseMap.addTo(map);
 
 var customLayer = L.geoJson(null, {
+    pointToLayer: function(feature, latlng){
+    var category = feature.properties.categories;
+    var inperson_n_h = feature.properties.inperson_n_h;
+    var radius = 8;
+
+    var markerOptions = {
+      radius: radius,
+      color: 'darkgrey',
+      weight: 0.5,
+      fillColor: '#f00',
+      fillOpacity: 1
+    };
+
+    // if (eventsInPerson.hasLayer(this)) {
+    //   radius = 8,
+    //   // markerOptions.fillColor = '#00f'; // Blue
+    //   markerOptions.fillOpacity= 0.3; // Transparent
+    // }
+    // if (eventsHybrid.hasLayer(this)) {
+    //   radius = 24,
+    //   // markerOptions.fillColor = '#800'; // Red
+    //   markerOptions.fillOpacity= 0.3; // Transparent
+    // }
+
+    if (category == "Pets") {
+      markerOptions.fillColor = '#0f0'; // Green
+    } else if (category == "Parents"){
+      markerOptions.fillColor = '#ff0'; // Yellow
+    } else if (category == "Spouses / Significant Others"){
+      markerOptions.fillColor = '#f80'; // Orange
+    } else if (category == "Children"){
+      markerOptions.fillColor = '#81a'; // Purple
+    } else if (category == "Friends") {
+      markerOptions.fillColor = '#8bf'; // Light Blue
+    } else if (category == "Other") {
+      markerOptions.fillColor = '#60f'; // Bluer purple
+    }
+
+    if (inperson_n_h == "h"){
+      markerOptions.className ='shadow';
+    }
+    return L.circleMarker(latlng, markerOptions);
+    },
   onEachFeature: function(feature, layer) {
 
     // Categories
@@ -134,10 +212,12 @@ var customLayer = L.geoJson(null, {
   }
 
     allEvents.addLayer(layer);
+    // console.log("All points added to All Events layer.");
 
     // Categories separation
     if (category == "Pets") {
       eventsPets.addLayer(layer);
+      // console.log("Pet Events layer populated.");
     } else if (category == "Parents"){
       eventsParents.addLayer(layer);
     } else if (category == "Spouses / Significant Others"){
@@ -153,9 +233,22 @@ var customLayer = L.geoJson(null, {
     // In-person / Hybrid separation
     if (inperson_n_h == "n") {
       eventsInPerson.addLayer(layer);
+      // console.log("In Person");
     } else if (inperson_n_h == "h") {
       eventsHybrid.addLayer(layer);
+      // console.log("Hybrid");
     }
+
+    layerSupport.addTo(map);
+    layerSupport.checkIn(allEvents);
+    layerSupport.checkIn(eventsPets);
+    layerSupport.checkIn(eventsParents);
+    layerSupport.checkIn(eventsSpouses);
+    layerSupport.checkIn(eventsChildren);
+    layerSupport.checkIn(eventsFriends);
+    layerSupport.checkIn(eventsOther);
+    layerSupport.checkIn(eventsInPerson);
+    layerSupport.checkIn(eventsHybrid);
   }
 });
 
@@ -170,30 +263,38 @@ var runLayer = omnivore.csv('./test_data.csv', null, customLayer)
       "Stamen Watercolor": Stamen_Watercolor
     };
 
+    petsCircle = "<svg class='circle' width='10' height='10'> <circle cx='5' cy='5' r='4' stroke-width='0.5' stroke='darkgrey' fill='#0f0'/></svg>";
+    parentsCircle = "<svg class='circle' width='10' height='10'> <circle cx='5' cy='5' r='4' stroke-width='0.5' stroke='darkgrey' fill='#ff0'/></svg>";
+    spousesCircle = "<svg class='circle' width='10' height='10'> <circle cx='5' cy='5' r='4' stroke-width='0.5' stroke='darkgrey' fill='#f80'/></svg>";
+    childrenCircle = "<svg class='circle' width='10' height='10'> <circle cx='5' cy='5' r='4' stroke-width='0.5' stroke='darkgrey' fill='#81a'/></svg>";
+    friendsCircle = "<svg class='circle' width='10' height='10'> <circle cx='5' cy='5' r='4' stroke-width='0.5' stroke='darkgrey' fill='#8bf'/></svg>";
+    otherCircle = "<svg class='circle' width='10' height='10'> <circle cx='5' cy='5' r='4' stroke-width='0.5' stroke='darkgrey' fill='#60f'/></svg>";
+    hybridCircle = "<svg class='shadow' width='10' height='10'> <circle cx='5' cy='5' r='4' stroke-width='0.5' stroke='darkgrey' fill='white'/></svg>";
+
     var overlayMaps = {
       "All Events": allEvents,
-      "Pets": eventsPets,
-      "Parents": eventsParents,
-      "Spouses / Significant Others": eventsSpouses,
-      "Children": eventsChildren,
-      "Friends": eventsFriends,
-      "Other": eventsOther,
+      [petsCircle + "Pets"]: eventsPets,
+      [parentsCircle + "Parents"]: eventsParents,
+      [spousesCircle + "Spouses / Significant Others"]: eventsSpouses,
+      [childrenCircle + "Children"]: eventsChildren,
+      [friendsCircle + "Friends"]: eventsFriends,
+      [otherCircle + "Other"]: eventsOther,
       "In-Person": eventsInPerson,
-      "Hybrid": eventsHybrid
+      [hybridCircle + "Hybrid (Virtual Option)"]: eventsHybrid
     };
 
     var layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed:true}).addTo(map);
     // layerControl.addOverlay(allEvents, "All Events") // Add "All Events" layer to the control manually
 
-    // Find the input element for the "All Events" overlay and set its checked property to true
-    var inputs = document.getElementsByClassName('leaflet-control-layers-overlays')[0].getElementsByTagName('input');
-    for (var i = 0; i < inputs.length; i++) {
-      var label = inputs[i].parentNode;
-      if (label.textContent.trim() === 'All Events') {
-        inputs[i].checked = true;
-        break;
-      }
-    }
+    // // Find the input element for the "All Events" overlay and set its checked property to true
+    // var inputs = document.getElementsByClassName('leaflet-control-layers-overlays')[0].getElementsByTagName('input');
+    // for (var i = 0; i < inputs.length; i++) {
+    //   var label = inputs[i].parentNode;
+    //   if (label.textContent.trim() === 'All Events') {
+    //     inputs[i].checked = true;
+    //     break;
+    //   }
+    // }
 })
   .addTo(map);
 
